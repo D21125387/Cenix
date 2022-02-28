ad595211c39a0ad782188f99f8f9e9b159e36d54
D21125387 <D21125387@mytudublin.ie>
2022-02-28 08:40:56 +0000

    Changes

--- js/typewriter.js
+++ js/typewriter.js
@@ -4,10 +4,10 @@ var typewriter = new Typewriter(app, {
     loop: true
 });
 
-typewriter.typeString('<span class="title">Developer</span>')
+typewriter.typeString('<span class="text-indigo-300">Developer</span>')
     .pauseFor(2500)
     .deleteAll()
-    .typeString('<span class="title">Designer</span>')
+    .typeString('<span class="text-indigo-300">Designer</span>')
     .pauseFor(2500)
     .deleteAll()
     .start();
